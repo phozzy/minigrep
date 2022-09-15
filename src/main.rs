d5e5c852db11d54958ac8ade313bda9ca45f28f6
phozzy <arthur.fayzullin@gmail.com>
2022-09-15 19:33:59 +0200

    Use a struct to store arguments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 use std::{env, fs};
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let querry = &args[1];
-    let file_path = &args[2];
+struct Config {
+    querry: String,
+    file_path: String,
+}
 
-    (querry, file_path)
+fn parse_config(args: &[String]) -> Config {
+    let querry = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { querry, file_path }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (querry, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", querry);
-    println!("In the file {}", file_path);
+    println!("Searching for {}", config.querry);
+    println!("In the file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Shoule have been able to read the file");
 
     println!("With text:\n{contents}");
