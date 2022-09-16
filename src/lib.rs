11740060f5fcd2ae8ad0780afbdd9b28f4fef852
phozzy <arthur.fayzullin@gmail.com>
2022-09-16 23:27:16 +0200

    Move struct and functions to a library

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config {
+    pub querry: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments!");
+        }
+
+        let querry = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { querry, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,6 @@
-use std::{env, fs, process};
-use std::error::Error;
+use std::{env, process};
 
-struct Config {
-    querry: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments!");
-        }
-
-        let querry = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { querry, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -38,7 +13,7 @@ fn main() {
     println!("Searching for {}", config.querry);
     println!("In the file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
