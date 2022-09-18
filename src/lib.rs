f7bd46771fe537eebe2737513a0382271d681dd8
phozzy <arthur.fayzullin@gmail.com>
2022-09-18 10:44:54 +0200

    Implement search function

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,31 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+pub fn search<'a>(querry: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(querry) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let querry = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(querry, contents));
+    }
+}
